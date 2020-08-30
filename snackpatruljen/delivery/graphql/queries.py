ed19bbf8b0c1a4392eec38204b2f216015777209
Bjarke Sporring <work.bjarke@gmail.com>
2020-08-30 20:10:18 +0200

    Cleaned up and added proper resolver

--- snackpatruljen/delivery/graphql/queries.py
+++ snackpatruljen/delivery/graphql/queries.py
@@ -1,4 +1,12 @@
+from snackpatruljen.delivery.models import Delivery
 import graphene
 
+
 class DeliveryQuery(graphene.ObjectType):
-    
+    delivery = graphene.Field(
+        Delivery,
+        description="Deliver for order"
+    )
+
+    def resolve_delivery(self, info, delivery_id):
+        return Delivery.objects.get(pk=delivery_id)
