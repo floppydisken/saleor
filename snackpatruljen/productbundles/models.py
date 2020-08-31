ba04ad08e3a6576cd6b5dfda53de75c3c2d68c48
Bjarke Sporring <work.bjarke@gmail.com>
2020-08-31 20:46:06 +0200

    Added initial model

---
+++ snackpatruljen/productbundles/models.py
@@ -0,0 +1,20 @@
+from saleor.product.models import Collection
+from saleor import settings
+
+from django.db import models
+from django_prices.models import MoneyField
+
+
+class ProductBundle(Collection):
+    currency = models.CharField(max_length=3, default="DKK")
+    fixed_price_amount = models.DecimalField(
+        max_digits=settings.DEFAULT_MAX_DIGITS,
+        decimal_places=settings.DEFAULT_DECIMAL_PLACES,
+        default=0,
+        editable=False
+    )
+    fixed_price = MoneyField(
+        amount_field="fixed_price_amount",
+        currency_field="",
+    )
+    pass
